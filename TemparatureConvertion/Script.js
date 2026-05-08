3acd8adf5f422a3617c1e00af9b90238a52119c7
Girish-Codes <rajgirishraj9@gmail.com>
2026-05-08 16:33:17 +0530

    TemparatureConvertion

---
+++ TemparatureConvertion/Script.js
@@ -0,0 +1,21 @@
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsious = document.getElementById("toCelsious");
+const result = document.getElementById("result");
+
+let temp;
+
+function convert() {
+
+    if(toFahrenheit.checked) {
+        temp = Number(textBox.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) +"°F";
+    } else if(toCelsious.checked) {
+        temp = Number(textBox.value);
+        temp = temp - 32 * 5 / 9;
+        result.textContent = temp.toFixed(1) +"°C";
+    } else {
+        result.textContent = "Select Conversion Type";
+    }
+}
